6210cd20d0481e3b18cb41397ba01d28c6795de2
theSoenke <theSoenke@users.noreply.github.com>
2016-04-17 22:21:11 +0200

    Change lighbulb emission

--- Assets/Scripts/LightFlicker.cs
+++ Assets/Scripts/LightFlicker.cs
@@ -3,43 +3,38 @@
 public class LightFlicker : MonoBehaviour
 {
     public float timeOff = 0.5f;
+    public Vector2 randomTimeRange;
+    public Color offColor;
+    public Color onColor;
 
     private float changeTime;
     private float timeOn;
     private Light lamp;
+    private Renderer renderer;
 
 
     void Start()
     {
         lamp = GetComponent<Light>();
-        timeOn = Random.Range(0.5f, 5);
+        renderer = GetComponentInParent<Renderer>();
+        timeOn = Random.Range(randomTimeRange.x, randomTimeRange.y);
     }
 
     void Update()
     {
-        /*if (Time.time > changeTime)
+        if (Time.time > changeTime)
         {
             lamp.enabled = !lamp.enabled;
             if (lamp.enabled)
             {
                 changeTime = Time.time + timeOn;
-                timeOn = Random.Range(0, 5);
+                timeOn = Random.Range(randomTimeRange.x, randomTimeRange.y);
+                renderer.sharedMaterial.SetColor("_EmissionColor", onColor);
             }
             else
             {
                 changeTime = Time.time + timeOff;
-            }
-        }*/
-
-        if (Random.value > 0.95) //a random chance
-        {
-            if (lamp.enabled == true) //if the light is on...
-            {
-                lamp.enabled = false; //turn it off
-            }
-            else
-            {
-                lamp.enabled = true; //turn it on
+                renderer.sharedMaterial.SetColor("_EmissionColor", offColor);
             }
         }
     }
