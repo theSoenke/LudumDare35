717083ca47b144bb581d84ecc1155b2060bad149
theSoenke <theSoenke@users.noreply.github.com>
2016-04-17 11:24:31 +0200

    Add camera switcher

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    private List<Transform> cameras;
+    private int selection;
+
+    void Start()
+    {
+        cameras = new List<Transform>();
+
+        foreach (Transform child in transform)
+        {
+            cameras.Add(child);
+            child.gameObject.SetActive(false);
+        }
+
+        cameras[0].gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            cameras[selection].gameObject.SetActive(false);
+            selection++;
+            selection %= cameras.Count;
+            cameras[selection].gameObject.SetActive(true);
+        }
+    }
+}
