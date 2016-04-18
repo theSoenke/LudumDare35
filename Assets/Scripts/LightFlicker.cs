73309042b35d120233e43cc79bcb025fd8d6bb96
kautschuckflasche <nilsw.agner@web.de>
2016-04-18 17:24:53 +0200

    lightbulb sound

--- Assets/Scripts/LightFlicker.cs
+++ Assets/Scripts/LightFlicker.cs
@@ -6,6 +6,7 @@ public class LightFlicker : MonoBehaviour
     public Vector2 randomTimeRange;
     public Color offColor;
     public Color onColor;
+    public AudioSource sound;
 
     private float changeTime;
     private float timeOn;
@@ -15,6 +16,7 @@ public class LightFlicker : MonoBehaviour
 
     void Start()
     {
+        sound.Pause();
         lamp = GetComponent<Light>();
         render = GetComponentInParent<Renderer>();
         timeOn = Random.Range(randomTimeRange.x, randomTimeRange.y);
@@ -27,12 +29,14 @@ public class LightFlicker : MonoBehaviour
             lamp.enabled = !lamp.enabled;
             if (lamp.enabled)
             {
+                sound.Pause();
                 changeTime = Time.time + timeOn;
                 timeOn = Random.Range(randomTimeRange.x, randomTimeRange.y);
                 render.sharedMaterial.SetColor("_EmissionColor", onColor);
             }
             else
             {
+                sound.UnPause();
                 changeTime = Time.time + timeOff;
                 render.sharedMaterial.SetColor("_EmissionColor", offColor);
             }
